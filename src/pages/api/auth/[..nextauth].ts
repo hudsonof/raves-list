4d61e50acde968c0052995b221e044b96d94da46
Hudson Oliveira <hudson.o.f@gmail.com>
2022-03-24 23:20:26 -0300

    ajustes no nextauth

--- src/pages/api/auth/[..nextauth].ts
+++ src/pages/api/auth/[..nextauth].ts
@@ -4,7 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+            clientId: process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID : '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ? process.env.GOOGLE_CLIENT_SECRET : ''
+        }),
+    ],
 })
